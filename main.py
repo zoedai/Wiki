d1d15cb118118787cb8e4d7007e2e11637f2f759
M Dai <mengshud@gmail.com>
2015-05-16 22:07:04 -0700

    1.5

--- main.py
+++ main.py
@@ -1,18 +1,4 @@
-#!/usr/bin/env python
-#
-# Copyright 2007 Google Inc.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
+
 #
 import os
 import re
@@ -23,6 +9,7 @@ import hashlib
 import hmac
 import random
 import string
+import json
 
 from google.appengine.ext import db
 
@@ -31,20 +18,16 @@ jinja_env = jinja2.Environment(loader = jinja2.FileSystemLoader(template_dir),
 	extensions = ['jinja2.ext.autoescape'],
 	autoescape = True)
 
-secret = 'stars'
-# css_url = os.path.join(os.path.dirname(__file__), 'css/main.css')
-PAGETITLE = "Blog 1.4"
-nav_bar_list = [
-{"href": "/", "caption":"Home"},
-{"href": "/newpost", "caption":"New Post"},
-{"href": "/signup", "caption":"Sign up"},
-{"href": "/login", "caption": "Log in"},
+secret = 'starsmydestination'
+PAGETITLE = "Blog 1.5"
+nav_bar_list = [{"href": "/blog", "caption":"Home"},
+{"href": "/blog/newpost", "caption":"New Post"},
 ]
 
 
 def render_str(template, **params):
 	t = jinja_env.get_template(template)
-	return t.render(params, pagetitle = PAGETITLE, nav_bar_list = nav_bar_list )
+	return t.render(params)
 
 
 class Handler(webapp2.RequestHandler):
@@ -52,16 +35,17 @@ class Handler(webapp2.RequestHandler):
         self.response.out.write(*a, **kw)
 
     def render_str(self, template, **params):
-        return render_str(template, **params)
+		params['sitename'] = PAGETITLE
+		params['nav_bar_list'] = nav_bar_list
+		params['user'] = self.user
+		return render_str(template, **params)
 
     def render(self, template, **kw):
         self.write(self.render_str(template, **kw))
 
     def set_secure_cookie(self, name, val):
     	cookie_val = encrypt_cookie(val)
-    	self.response.headers.add_header(
-    		'Set-Cookie',
-    		'%s=%s; Path = /' % (name, cookie_val))
+    	self.response.set_cookie(name, cookie_val, path='/')
 
     def read_secure_cookie(self, name):
     	cookie_val = self.request.cookies.get(name)
@@ -70,22 +54,53 @@ class Handler(webapp2.RequestHandler):
     def login(self, user):
     	self.set_secure_cookie('user_id', str(user.key().id()))
 
+    def logout(self):
+    	self.response.delete_cookie('user_id')
+
+    def render_json(self, d):
+	    json_txt = json.dumps(d)
+	    self.response.headers['Content-Type'] = 'application/json; charset=UTF-8'
+	    self.write(json_txt)
+
+    # copy
+    def initialize(self, *a, **kw):
+        webapp2.RequestHandler.initialize(self, *a, **kw)
+        uid = self.read_secure_cookie('user_id')
+        self.user = uid and User.by_id(int(uid))
+
+        if self.request.url.endswith('.json'):
+            self.format = 'json'
+        else:
+            self.format = 'html'
+
 
 class Post(db.Model):
-	title = db.StringProperty(required = True)
+	subject = db.StringProperty(required = True)
 	content = db.TextProperty(required = True)
 	created = db.DateTimeProperty(auto_now_add = True)
 	last_modified = db.DateTimeProperty(auto_now = True)
+
+	def as_dict(self):
+	    time_fmt = '%c'
+	    d = {'subject': self.subject,
+	         'content': self.content,
+	         'created': self.created.strftime(time_fmt),
+	         'last_modified': self.last_modified.strftime(time_fmt)}
+	    return d
 
 	def render(self):
 		self._render_text = self.content.replace('\n', '<br>')
 		return render_str("post.html", p = self)
 
+
+
+	    
+
 #### user stuff
 def make_salt():
-	return "".join(random.choice(string.ascii_letters) for _ in range(8))
+	return "".join(random.choice(string.letters) for _ in range(8))
 	
-def make_secure_pw(s, salt=""):
+def make_secure_pw(s, salt= None):
 	if not salt:
 		salt = make_salt()
 	return '%s|%s'%(salt, hashlib.sha256(s+salt).hexdigest())
@@ -94,12 +109,16 @@ def verify_pw(name, pw, hashed):
 	salt = hashed.split('|')[0]
 	return make_secure_pw(name+pw, salt) == hashed
 
-def encrypt_cookie(name):
-	return '%s|%s' % (name, hmac.new(name,secret).hexdigest())
+def encrypt_cookie(val):
+	return '%s|%s' % (val, hmac.new(str(val),secret).hexdigest())
 
 def verify_cookie(value):
 	val = value.split('|')[0]
-	return value == encrypt_cookie(val)
+	if value == encrypt_cookie(val):
+		return val
+
+
+
 
 class User(db.Model):
 	username = db.StringProperty(required = True)
@@ -121,13 +140,8 @@ class User(db.Model):
 		if u and verify_pw(name, pw, u.password):
 			return u
 
-
-class MainHandler(Handler):
-	def get(self):
-		posts = db.GqlQuery("SELECT * FROM Post "
-						   "ORDER BY created DESC")
-		self.render("main.html", posts = posts, currentPage = "Home"
-		)
+def users_key(group = 'default'):
+	return db.Key.from_path('users', group)
 
 class PostHandler(Handler):
 	def get(self, post_id):
@@ -136,25 +150,41 @@ class PostHandler(Handler):
 		if not p:
 			self.error(404)
 			return
+		if self.format == 'html':
+			self.render("permlink.html", p = p)
+		else:
+			self.render_json(p.as_dict())
 
-		self.render("permlink.html", p = p)
+class BlogFront(Handler):
+	def get(self):
+		posts = db.GqlQuery("SELECT * FROM Post "
+						   "ORDER BY created DESC")
+		if self.format == 'html':
+			self.render("main.html", posts = posts, currentPage = "Home")
+		else:
+			self.render_json([p.as_dict() for p in posts])
 
 class NewPost(Handler):
+	# def render_newpost(self, title = "", 
+	# 	content = "", error = ""):
+	# 	self.render("newpost.html", pagetitle = "New Post", 
+	# 		title = title, content = content, error = error)
 	def get(self):
-		self.render("newpost.html", currentPage = "New Post")
+		self.render("newpost.html", pagetitle = "Write a new post!", currentPage = "New Post")
 
 	def post(self):
 		title = self.request.get("subject")
 		content = self.request.get("content")
 
 		if title and content:
-			a = Post(title = title, content = content)
+			a = Post(subject = title, content = content)
 			a.put()
-			self.redirect("/"+str(a.key().id()))
+			self.redirect("/blog/"+str(a.key().id()))
 		else:
 			error = "we need both a title and some words!"
 			self.render_newpost(title, content, error)
 
+# some validation (from teacher)
 USER_RE = re.compile(r"^[a-zA-Z0-9_-]{3,20}$")
 def valid_username(username):
     return username and USER_RE.match(username)
@@ -202,10 +232,11 @@ class SignUp(Handler):
 		else:
 			hashed = make_secure_pw(self.username+self.password)
 			usr = User(username = self.username,
-			 password = hashed, email = self.email)
+			 password = hashed, email = self.email,
+			 parent = users_key())
 			usr.put()
 			self.login(usr)
-			self.redirect('/welcome?user='+self.username)
+			self.redirect('/blog/welcome?user='+self.username)
 
 
 	def get(self, **kw):
@@ -220,7 +251,7 @@ class SignIn(Handler):
 		u = User.login(username, password)
 		if u:
 			self.login(u)
-			self.redirect('/')
+			self.redirect('/blog')
 		else:
 			self.get("invalid user name or password")
 
@@ -235,17 +266,20 @@ class Welcome(Handler):
 
 class LogOut(Handler):
 	def get(self):
-		self.response.headers.add_header('Set-Cookie','username =;Path=/')
-		self.redirect('/signup')
+		self.logout()
+		self.redirect('/blog')
 
 			
 
+
 app = webapp2.WSGIApplication([
-    ('/', MainHandler),
-    ('/newpost', NewPost),
-    ('/signup', SignUp),
-    ('/welcome',Welcome),
-    ('/login', SignIn),
-    ('/logout', LogOut),
-    (r'/(\d+)', PostHandler)
+    # ('/blog', BlogHandler),
+    ('/blog/newpost', NewPost),
+    ('/blog/?(?:\.json)?', BlogFront),
+    ('/blog/([0-9]+)(?:\.json)?', PostHandler),
+    ('/blog/signup', SignUp),
+    ('/blog/welcome',Welcome),
+    ('/blog/login', SignIn),
+    ('/blog/logout', LogOut),
+    # (r'/(\d+)', PostHandler)
 ], debug=True)
